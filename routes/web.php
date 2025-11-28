d238cef9bd3c8303a91e88b1c488a43a68a1fdf9
DevMo.21x <DevMo.21x@outlook.com>
2025-11-28 00:12:54 -0400

    Basic authentication video done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 Route::get('/', [ChirpController::class, 'index']);
@@ -24,5 +25,17 @@ Route::view('/register', 'auth.register')
 Route::post('/register', Register::class)
     ->middleware('guest');
 
+
+
+// Login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+// Logout route
 Route::post('/logout', Logout::class)
-    ->middleware('auth');
+    ->middleware('auth')
+    ->name('logout');
